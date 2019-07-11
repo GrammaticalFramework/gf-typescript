1f8da2cf0884871fdc10d6261dda46c1b706e1c5
John J. Camilleri <john@digitalgrammars.com>
2019-07-11 09:14:49 +0200

    Add basic linearisation tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,20 +1,49 @@
-import { fromJSON, GFGrammar } from '../src/index'
-import grammar from './grammars/ZeroModule'
+import { fromJSON, GFGrammar, Fun } from '../src/index'
+import grammarJS from './grammars/ZeroModule'
 
 import 'mocha'
 import * as should from 'should'
 import { readFileSync } from 'fs'
 
+var grammar: GFGrammar
+
+before((): void => {
+  let json = JSON.parse(readFileSync('./test/grammars/Zero.json').toString())
+  let g = fromJSON(json)
+  if (g) grammar = g as GFGrammar
+})
+
 describe('Importing', (): void => {
 
-  it('should import from JS', (): void => {
+  it('imports from JS', (): void => {
+    should.exist(grammarJS)
+  })
+
+  it('imports from JSON', (): void => {
     should.exist(grammar)
   })
 
-  it('should import from JSON', (): void => {
-    let json = JSON.parse(readFileSync('./test/grammars/Zero.json').toString())
-    let grammar: GFGrammar | null = fromJSON(json)
-    should.exist(grammar)
+})
+
+describe('Linearisation', (): void => {
+
+  let treeS = 'eat apple'
+  let tree: Fun
+
+  it('parses tree', (): void => {
+    let t = grammar.abstract.parseTree(treeS)
+    should.exist(t)
+    if (t) tree = t as Fun
+  })
+
+  it('linearises in English', (): void => {
+    let s = grammar.concretes['ZeroEng'].linearize(tree)
+    s.should.equal('eat an apple')
+  })
+
+  it('linearises in Swedish', (): void => {
+    let s = grammar.concretes['ZeroSwe'].linearize(tree)
+    s.should.equal('äta ett äpple')
   })
 
 })
